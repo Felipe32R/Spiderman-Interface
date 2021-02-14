644bf2418c9dab48a8aadfd037f510135a4fb16d
Felipe Ramalho <feliperamalhoy@gmail.com>
2021-02-14 18:01:26 -0300

    PS5 Spiderman Game

---
+++ js/app.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  let tl = new TimelineMax();
+
+  tl.fromTo('.bg-loader', 1 ,
+  {width:'100%'},
+  {width:'0%',delay: 4,ease: Expo.easeInOut})
+
+  .fromTo('.bg-video', 2,
+  {width:'0%',opacity: 0},
+  {width:'100%', opacity: 1, ease: Expo.easeInOut}, '-=1')
+
+  .fromTo('.container-item1', 0.7,
+  {width:'0%',opacity: 0},
+  {width:'100%', opacity: 1, ease: Expo.easeInOut}, '-=0.3')
+
+  .fromTo('.container-item2', 0.7,
+  {x:50,opacity: 0},
+  {x:0, opacity: 1, ease: Expo.easeInOut}, '-=0.3')
+
+  .fromTo('.logo', 0.7,
+  {y:-50,opacity: 0},
+  {y:0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+  .fromTo('.nav-list', 0.7,
+  {y:-50,opacity: 0},
+  {y:0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+  .fromTo('.nav-social', 0.7,
+  {y:-50,opacity: 0},
+  {y:0, opacity: 1, ease: Expo.easeInOut}, '-=0.5')
+
+  
+  
+
+});
